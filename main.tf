7901c7f4b4a454c67361c7df602a5e1ce666962d
unknown <ambarbhore5757@gmail.com>
2026-09-07 19:26:22 +0530

    Adding one more tag for app

--- main.tf
+++ main.tf
@@ -5,8 +5,10 @@ provider "aws" {
 resource "aws_vpc" "dev" {
   cidr_block = var.vpc_cidr
   tags = {
-    Name = "Devlop enviornment VPC"
-    Env = "Dev"
-    vpc = "dev vpc"
+    Name  = "Devlop enviornment VPC"
+    Env   = "Dev"
+    vpc   = "dev vpc"
+    Owner = "Ambar"
+    App   = "Payment-Gateway"
   }
 }
